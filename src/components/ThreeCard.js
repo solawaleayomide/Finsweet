ec03edd18493842b6956f818f72d24ac5bb026bc
solawaleayomide <olawalesamuel147@gmail.com>
2025-01-21 11:14:46 +0100

    finsweet

--- src/components/ThreeCard.js
+++ src/components/ThreeCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Three({ heading, desc, image }) {
   return (
-    <div>
+    <div className="mb-[3rem]">
       <img src={image} alt="img" className="w-[1.5rem]" />
       <h2 className="font-bold text-[1.3rem] mt-5 mb-2"> {heading}</h2>
       <p className="text-[#5B5B5B]">{desc}</p>
