07f45d3fc707fe9640b86825b310a72a8392a54c
solawaleayomide <olawalesamuel147@gmail.com>
2025-01-21 19:15:43 +0100

    finsweet

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -5,9 +5,11 @@ function BlogPost({ image, text1, text2, text3 }) {
     <div>
       <div className="mb-[3rem] text-center md:text-start bg-white rounded-lg shadow">
         <img src={image} alt="postImg" className="w-full h-auto rounded-md" />
-        <h2 className="my-[1rem] font-bold text-[1.2rem]">{text1}</h2>
-        <p className="mb-[1rem]">{text2}</p>
-        <p className="font-semibold">{text3}</p>
+        <div className="p-4">
+          <h2 className="my-[1rem] font-bold text-[1.2rem]">{text1}</h2>
+          <p className="mb-[1rem]">{text2}</p>
+          <p className="font-semibold">{text3}</p>
+        </div>
       </div>
     </div>
   );
